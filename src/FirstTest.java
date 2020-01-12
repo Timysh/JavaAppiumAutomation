5e62b8f6644f4a489def393eda8e603d65f56250
Timysh <strategtim@gmail.com>
2020-01-12 13:19:18 +0300

    Ответ на Ex1: Конфигурация инструментов

---
+++ src/FirstTest.java
@@ -0,0 +1,40 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp () throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("app", "C:/Users/User/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest(){
+        System.out.println("First test run");
+    }
+
+}
