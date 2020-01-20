f6f671ef99c7696532e4c9feb8f55911b22124c4
Timysh <strategtim@gmail.com>
2020-01-20 22:26:11 +0300

    Создан тест testSwipeArticle. Созданы методы swipeUp, swipeUpQuick и swipeUpToFindElement

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -139,6 +141,38 @@ public class FirstTest {
 
     }
 
+    @Test
+    public void testSwipeArticle() {
+
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input ",
+                5);
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "'Search Wikipedia' is not shown",
+                5);
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"),
+                "Appium",
+                "Text cannot be send",
+                5);
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
+                "Cannot find 'Appium' article",
+                15);
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "'Search Wikipedia' is not shown",
+                15);
+
+        swipeUpToFindElement(
+                By.xpath("//*[@text='View page in browser']"),
+                "Cannot find the end of the article",
+                20);
+    }
+
     //Ожидание появления элемента на экране
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds){
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
@@ -179,4 +213,41 @@ public class FirstTest {
         return element;
     }
 
+    //Свайп экрана снизу вверх
+    protected void swipeUp(int timeOfSwipe){
+        TouchAction action = new TouchAction(driver);
+        //Получаем размер экрана по осям X и Y
+        Dimension size = driver.manage().window().getSize();
+        //Координаты первоночального нажатия на экран по горизонтали
+        int x = size.width / 2;
+        //Координаты первоночального нажатия на экран по вертикали
+        int start_y = (int) (size.height * 0.8);
+        //Координаты свайпа для конечной точки по вертикали
+        int end_y = (int) (size.height * 0.2);
+
+        //Метод для свайпа снизу вверх. Переменная timeOfSwipe отвечает за скорость свайпа
+        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+    }
+
+    //Быстрый свайп
+    protected void swipeUpQuick(){
+        swipeUp(200);
+    }
+
+    //Свайп до определенного элемента
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes){
+        int already_swiped = 0;
+        while (driver.findElements(by).size() == 0){
+
+            if (already_swiped > max_swipes){
+                //Метод отвечает за то, если элемент не найден, тогда тест упадет
+                waitForElementPresent(by, "Cannot find element by swiping up. \n" + error_message, 0);
+                //Выходим из функции, если элемент найден.
+                return;
+            }
+
+            swipeUpQuick();
+            ++already_swiped;
+        }
+    }
 }
