2beae942f9092e5a9169e6f2c558ccccb24ebce4
Timur Zhanchikov <tzhanchikov@usetech.ru>
2020-01-24 11:58:19 +0300

    Ответ на задание Ex7*: Поворот экрана

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -35,6 +35,7 @@ public class FirstTest {
         capabilities.setCapability("app", "C:/Users/tzhanchikov/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
     @After
